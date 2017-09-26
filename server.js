df1b8104d350a08784287d017e22be90686f9ddc
Lina Yang <t886515@hotmail.com>
2017-09-26 15:32:34 -0500

    Set-up basic package environment and server routing

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// mongoose.connect('mongodb://localhost/personalWebsite');
+
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'angular-client')));
+
+app.use(function(req, res, next) {
+  console.log('Serving ' + req.method + 'for ' + req.url);
+  next();
+});
+
+app.get('/', ()=>{
+
+})
+
+app.use('/style.css', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'style.css'));
+})
+
+app.listen(6666, ()=>{
+  console.log('Listening on port 6666')
+})
