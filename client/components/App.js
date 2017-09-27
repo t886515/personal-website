23866f41c059468764c7f4d4e4b85864d24884b5
Lina Yang <t886515@hotmail.com>
2017-09-27 18:49:52 -0500

    Set-up index.html page. Page initiated with angular.js framework.

---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+angular.module('personal-website', [])
+
+.controller('AppCtrl', function() {
+
+})
+
+.component('app', {
+  controller: 'AppCtrl',
+  templateUrl: 'templates/app.html'
+})
--- server.js
+++ server.js
@@ -12,21 +12,21 @@ app.use(bodyParser.json());
 // mongoose.connect('mongodb://localhost/personalWebsite');
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-app.use(express.static(path.join(__dirname, 'angular-client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use(function(req, res, next) {
   console.log('Serving ' + req.method + 'for ' + req.url);
   next();
 });
 
-app.get('/', ()=>{
+// app.get('/', ()=>{
 
-})
+// })
 
 app.use('/style.css', (req, res, next)=>{
   res.sendFile(path.join(__dirname, 'style.css'));
 })
 
-app.listen(6666, ()=>{
-  console.log('Listening on port 6666')
+app.listen(8000, ()=>{
+  console.log('Listening on port 8000')
 })
